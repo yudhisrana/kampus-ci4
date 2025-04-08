c3cbbb97eb455efe667e5e704572d3ce3d0346b5
Dipo Yudhis Rana <dipoyudhisrana@gmail.com>
2025-04-08 19:58:22 +0700

    add header, sidebar, footer template

---
+++ app/Views/Backend/Template/footer.php
@@ -0,0 +1,26 @@
+<script src="Assets/js/jquery-1.11.1.min.js"></script>
+<script src="Assets/js/bootstrap.min.js"></script>
+<script src="Assets/js/chart.min.js"></script>
+<script src="Assets/js/chart-data.js"></script>
+<script src="Assets/js/easypiechart.js"></script>
+<script src="Assets/js/easypiechart-data.js"></script>
+<script src="Assets/js/bootstrap-datepicker.js"></script>
+<script src="Assets/js/bootstrap-table.js"></script>
+<script>
+  ! function($) {
+    $(document).on("click", "ul.nav li.parent > a > span.icon", function() {
+      $(this).find('em:first').toggleClass("glyphicon-minus");
+    });
+    $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+  }(window.jQuery);
+
+  $(window).on('resize', function() {
+    if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+  })
+  $(window).on('resize', function() {
+    if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+  })
+</script>
+</body>
+
+</html>
---
+++ app/Views/Backend/Template/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Lumino - Tables</title>
+
+  <link href="Assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="Assets/css/datepicker3.css" rel="stylesheet" />
+  <link href="Assets/css/bootstrap-table.css" rel="stylesheet" />
+  <link href="Assets/css/styles.css" rel="stylesheet" />
+
+  <!--[if lt IE 9]>
+			<script src="js/html5shiv.js"></script>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+</head>
+
+<body>
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"><span>Lumino</span>Admin</a>
+        <ul class="user-menu">
+          <li class="dropdown pull-right">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> User <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-user"></span> Profile</a>
+              </li>
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-cog"></span> Settings</a>
+              </li>
+              <li>
+                <a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- /.container-fluid -->
+  </nav>
